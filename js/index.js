2bc541d8f93b2d96e63066e8561abfb976621ad6
William Jack Boening <wjbz82@mail.missouri.edu>
2015-12-20 01:52:51 -0600

    Simple TODO App

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function TodoCtrl($scope) {
+  
+  $scope.todos = [
+  ];
+  
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+  
+  
+  $scope.addTodo = function () {
+    $scope.todos.push({text:$scope.formTodoText, done:false});
+    $scope.formTodoText = '';
+  };
+  
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+}
